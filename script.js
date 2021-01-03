1411b5ce3b973e4e7c4b5a86b8a1637390f64f71
Arda Göktaş <ardagoktas2004@gmail.com>
2021-01-03 21:19:26 +0200

    First Project of 2021

---
+++ script.js
@@ -0,0 +1,86 @@
+let Question = function(text, choices, answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    
+}
+//Question Prototype
+Question.prototype.checkAnswer = function(answer){
+    return this.answer === answer;
+}
+
+//Quiz Constructor
+let Quiz = function(questions){
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0;
+}
+//Quiz Prototype
+//getQuestion
+Quiz.prototype.getQuestion = function(){
+    return this.questions[this.questionIndex];
+}
+//isFinish
+Quiz.prototype.isFinish = function(){
+    return this.questions.length === this.questionIndex;
+}
+//Quiz guess
+Quiz.prototype.guess = function(answer){
+    var question = this.getQuestion();
+
+    if(question.checkAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
+var q1 = new Question("Who is the best basketball player of all time? ",["Lebron James", "Micheal Jordan", "Kobe Bryant", "Kevin Durant"],"Micheal Jordan");
+var q2 = new Question("Which basketball player has nickname called King? ",["Micheal Jordan", "Stephen Curry","Larry Bird","Lebron James"],"Lebron James");
+var q3 = new Question("Who is the best shooter of all time? ",["Micheal Jordan", "Stephen Curry","Larry Bird","Lebron James"],"Stephen Curry");
+var questions = [q1, q2, q3];
+
+//Start Quiz
+var quiz = new Quiz(questions);
+
+loadQuestion();
+
+function loadQuestion(){
+    if(quiz.isFinish()){
+        //showScore
+        showScore();
+    }else{
+        var question = quiz.getQuestion();
+        var choices = question.choices;
+        document.querySelector("#question").textContent = question.text;
+
+        for(var i=0; i<choices.length; i++){
+            var element = document.querySelector('#choice'+i);
+            element.innerHTML = choices[i];
+
+            guess('btn'+i,choices[i]);
+        }
+        showProgress();
+    }
+}
+function showScore(){
+    var html = `<h2>Score</h2><h4>${quiz.score}</h4>`;
+    document.querySelector('.card-body').innerHTML = html;
+
+}
+function guess(id, guess){
+    var btn = document.getElementById(id);
+    btn.onclick = function(){
+        quiz.guess(guess);
+        loadQuestion();
+    }
+}
+function showProgress(){
+    var totalQuestion = quiz.questions.length;
+    var questionNumber = quiz.questionIndex+1;
+    document.querySelector('#progress').innerHTML = 'Question'+ questionNumber + ' of ' + totalQuestion; 
+}
+
+btnAgain = document.querySelector('#tryAgain');
+btnAgain.onclick = function(){
+    window.location.reload(true);
+};
